89d4a5c40b3adc66e93858b973094ff4b321c013
Alex <sasha.k150189@mail.ru>
2021-12-29 15:19:22 +0300

    switch

--- HomeWork_Lesson_2/Program.cs
+++ HomeWork_Lesson_2/Program.cs
@@ -5,7 +5,7 @@ namespace HomeWork_Lesson_2
     class Program
     {
         static void Main(string[] args)
-        {
+        {/*
             #region Task 1 Condition with three points
             int a, b, max;
             Console.WriteLine("Type two digits");
@@ -33,13 +33,47 @@ namespace HomeWork_Lesson_2
             }
             #endregion
 
-            #region Task 2 Тернарный оператор
+            #region Task 2 Тернарный оператор "?"
             int num;
             Console.WriteLine("Type digit ");
             num = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Digit " + num + ((num % 2 == 0) ? " even number" : " odd number"));
             #endregion
-
+            */
+            #region Task 3 Оператор выбора "Switch"
+            // Оператор switch предназначен для разветвления процесса выполнения программы
+            int month;
+            Console.WriteLine("Type number of month");
+            month = Convert.ToInt32(Console.ReadLine());
+            string s;
+            switch (month)
+            {
+                case 1:
+                case 2:
+                case 12:
+                    s = "It is Winter";
+                    break;
+                case 3:
+                case 4:
+                case 5:
+                    s = "It is Spring";
+                    break;
+                case 6:
+                case 7:
+                case 8:
+                    s = "It is Summer";
+                    break;
+                case 9:
+                case 10:
+                case 11:
+                    s = "It is Autumn";
+                    break;
+                default:
+                    s = "Nothing";
+                    break;
+            }
+            Console.WriteLine(s);
+            #endregion
 
         }
     }
